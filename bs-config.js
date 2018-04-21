e4586a02bc7d1f015cf3c6c1d16f8bf13087d217
Dias, José (Developer) <jose.dias@sky.uk>
2018-04-21 12:13:34 +0100

    working ecossystem

--- bs-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  server: {
-    middleware: {
-      0: null
-    }
-  }
-};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  mode: process.env.NODE_ENV || 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: 'dist/index.html'
+    }
+  },
+  performance: {
+    hints: false
+  }
+};
